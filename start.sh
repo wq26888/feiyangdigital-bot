631b3491eb6c760bd303a2ecbad2a6ddcd63c90c
wq26888 <101094690+wq26888@users.noreply.github.com>
2025-03-22 22:54:03 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # 定义下载路径
-DIR="/home/feiyangdigitalbotconf"
+DIR="/docker/feiyangdigitalbotconf"
 
 # 定义要下载的文件的URL
 URLS=(
-    "https://ghproxy.com/https://raw.githubusercontent.com/youshandefeiyang/feiyangdigital-bot/main/config/bot.sql"
-    "https://ghproxy.com/https://raw.githubusercontent.com/youshandefeiyang/feiyangdigital-bot/main/config/config.json"
-    "https://ghproxy.com/https://raw.githubusercontent.com/youshandefeiyang/feiyangdigital-bot/main/docker-compose.yml"
+    "https://hub.docker.com/r/youshandefeiyang/feiyangdigital-bot/main/config/bot.sql"
+    "https://hub.docker.com/r/youshandefeiyang/feiyangdigital-bot/main/config/config.json"
+    "https://hub.docker.com/r/youshandefeiyang/feiyangdigital-bot/main/docker-compose.yml"
 )
 
 # 检查目录是否存在，如果存在则删除并重新创建
@@ -26,4 +26,4 @@ for url in "${URLS[@]}"; do
 done
 
 # 所有文件下载完成后的提示
-echo -e "\e[31m请在/home/feiyangdigitalbotconf/config.json中填写你的bot配置信息\e[0m" # 使用红色字体显示提示信息
+echo -e "\e[31m请在/docker/feiyangdigitalbotconf/config.json中填写你的bot配置信息\e[0m" # 使用红色字体显示提示信息
